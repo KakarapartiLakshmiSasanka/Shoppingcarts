56aad1304b9f1c1c6a739c58a80eff6d4d2d178d
sasanka <sasankasrinivas99@gmail.com>
2022-09-14 15:56:04 +0530

    modified html

--- websockets/server.js
+++ websockets/server.js
@@ -1,21 +1,50 @@
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws');
-
-const port = 6969;
+const WebSocket = require("ws");
+const PORT = 3000;
 const server = http.createServer(express);
-const wss = new WebSocket.Server({ server })
+const wss = new WebSocket.Server({server});
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    })
-  })
-})
+wss.on('connection',function connection(ws){
+    ws.on('message',function incoming(message){
+        console.log("Received: "+message);
+        
+        wss.clients.forEach(function each(client){
+            if(client!==ws && client.readyState===WebSocket.OPEN)
+            {
+                client.send(JSON.stringify(message));
+            }
+        });
+    });
+});
 
-server.listen(port, function() {
-  console.log(`Server is listening on ${port}!`)
-})
+server.listen(PORT,()=>console.log(`Server is listening to PORT ${PORT}`));
+
+
+
+
+
+// const { json } = require('body-parser');
+// const express = require('express');
+// const http = require('http');
+// const WebSocket = require('ws');
+
+// const port = 6969;
+// const server = http.createServer(express);
+// const wss = new WebSocket.Server({ server })
+
+// wss.on('connection', function connection(ws) {
+//   ws.on('message', function incoming(data) {
+//     wss.clients.forEach(function each(client) {
+//       if (client !== ws && client.readyState === WebSocket.OPEN) {
+//         console.log('data %s', data);
+//         client.send(JSON.stringify(data));
+//         // client.send(data);
+//       }
+//     })
+//   })
+// })
+
+// server.listen(port, function() {
+//   console.log(`Server is listening on ${port}!`)
+// })
